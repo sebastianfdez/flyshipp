c10ab90253c9ea01f89a85590b5b666c71d55fea
Sebastian <sfernandez@clever-age.com>
2021-05-14 21:30:08 +0200

    create contact footer

---
+++ src/app/shared/component/fs-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fs-footer',
+  templateUrl: 'fs-footer.component.html'
+})
+
+export class FsFooterComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { ApiService } from './services/api.service';
 import { FirebaseModule } from './firebase.module';
+import { FsFooterComponent } from './component/fs-footer.component';
 
 @NgModule({
+  declarations: [
+    FsFooterComponent,
+  ],
   imports: [
     HttpClientModule,
     FirebaseModule,
@@ -13,6 +17,7 @@ import { FirebaseModule } from './firebase.module';
   ],
   exports: [
     FirebaseModule,
+    FsFooterComponent,
   ],
 })
 export class SharedModule { }
